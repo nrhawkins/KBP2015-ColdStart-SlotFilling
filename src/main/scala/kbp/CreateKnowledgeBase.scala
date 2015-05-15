da2bf0e93cd36399c1963d1c8e166121277f6fff
nrhawkins <nrhawkins@gmail.com>
2015-05-15 15:33:01 -0700

    Create CreateKnowledgeBase.scala

---
+++ src/main/scala/kbp/CreateKnowledgeBase.scala
@@ -0,0 +1,21 @@
+package kbp
+
+import scala.collection.JavaConversions._
+
+
+object CreateKnowledgeBase {
+
+  def main(args: Array[String]) {
+  
+    val runID = "UWashington1"
+    val detailed = false
+
+    //for(file <- myDirectory.listFiles if file.getName endsWith ".txt"){
+      // process the file
+    //}
+    
+    
+
+  }
+  
+}
